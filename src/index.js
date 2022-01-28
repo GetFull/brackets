9e6e447f6b8b2b5a51e379e2dce004852b9fe275
getfull <gtfullz@gmail.com>
2022-01-28 12:19:02 +0200

    feat: add sub-optimal solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let openBrackets = bracketsConfig.map(([open, close]) => open);
+  let closeBrackets = bracketsConfig.map(([open, close]) => close);
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let multi = openBrackets.includes(str[i]) && closeBrackets.includes(str[i]);
+    let stackTop = stack[stack.length - 1];
+    let current = str[i];
+
+    if (multi && stackTop === current) {
+      stack.pop();
+    } else if (openBrackets.includes(current)) {
+      stack.push(current);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      if (
+        stackTop ===
+        bracketsConfig.filter((item) => item.includes(current))[0][0]
+      ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
